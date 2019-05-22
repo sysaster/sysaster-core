666f2bddb4e4213135ac5516d84b59d92d6091ac
Vitor Greati <greati@ufrn.edu.br>
2019-05-22 17:25:39 -0300

    Remove unused settings arg

--- sources/include/sysaster/core/settings/JSONSettingsParser.h
+++ sources/include/sysaster/core/settings/JSONSettingsParser.h
@@ -11,7 +11,7 @@ void from_json(const json& j, Settings& s) {
     s.detection_pool_size = j["detection"]["pool_size"];
     s.connection_pool_size = j["connection"]["pool_size"];
     s.server_port = j["connection"]["server_port"];
-    s.server_ip = j["connection"]["server_ip"];
+    //s.server_ip = j["connection"]["server_ip"];
     s.image_source_interval = j["image_source"]["interval"];
     s.server_endpoint = j["connection"]["server_endpoint"];
     s.image_source_url = j["image_source"]["url"];
--- sources/include/sysaster/core/settings/Settings.h
+++ sources/include/sysaster/core/settings/Settings.h
@@ -8,7 +8,7 @@ struct Settings {
     int detection_pool_size {1};
     int connection_pool_size {1};
     size_t server_port;
-    std::string server_ip;
+    //std::string server_ip;
     std::string server_endpoint;
     std::string image_source_url;
     int image_source_interval {1};
